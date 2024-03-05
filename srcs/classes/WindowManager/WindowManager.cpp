a45b089963613c6022550a443e44b63c6ea9a709
averdon <anthonyverdon10@gmail.com>
2024-03-05 10:30:48 +0100

    draw one block

--- srcs/classes/WindowManager/WindowManager.cpp
+++ srcs/classes/WindowManager/WindowManager.cpp
@@ -58,26 +58,70 @@ void WindowManager::updateLoop()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
     float vertices[] = {
-        -0.5, -0.5, 0, 0, 0, //
-        0.5,  -0.5, 0, 1, 0, //
-        -0.5, 0.5,  0, 0, 1, //
-        0.5,  0.5,  0, 1, 1  //
+        // top
+        0.0, 1.0, 0.0, 0.0, 0.0, //
+        1.0, 1.0, 0.0, 0.5, 0.0, //
+        0.0, 1.0, 1.0, 0.0, 0.5, //
+        1.0, 1.0, 1.0, 0.5, 0.5, //
+
+        // sides
+        0.0, 0.0, 0.0, 0.0, 0.5, //
+        1.0, 0.0, 0.0, 0.5, 0.5, //
+        0.0, 1.0, 0.0, 0.0, 1.0, //
+        1.0, 1.0, 0.0, 0.5, 1.0, //
+
+        0.0, 0.0, 0.0, 0.0, 0.5, //
+        0.0, 1.0, 0.0, 0.0, 1.0, //
+        0.0, 0.0, 1.0, 0.5, 0.5, //
+        0.0, 1.0, 1.0, 0.5, 1.0, //
+
+        0.0, 0.0, 1.0, 0.0, 0.5, //
+        1.0, 0.0, 1.0, 0.5, 0.5, //
+        0.0, 1.0, 1.0, 0.0, 1.0, //
+        1.0, 1.0, 1.0, 0.5, 1.0, //
+
+        1.0, 0.0, 0.0, 0.0, 0.5, //
+        1.0, 1.0, 0.0, 0.0, 1.0, //
+        1.0, 0.0, 1.0, 0.5, 0.5, //
+        1.0, 1.0, 1.0, 0.5, 1.0, //
+
+        // bot
+        0.0, 0.0, 0.0, 0.5, 0.5, //
+        1.0, 0.0, 0.0, 1.0, 0.5, //
+        0.0, 0.0, 1.0, 0.5, 1.0, //
+        1.0, 0.0, 1.0, 1.0, 1.0, //
+
     };
 
     unsigned int faces[] = {
-        0, 1, 2, //
-        1, 2, 3  //
+        0,  1,  2, //
+        1,  2,  3, //
+
+        4,  5,  6, //
+        5,  6,  7, //
+
+        8,  9,  10, //
+        9,  10, 11, //
+
+        12, 13, 14, //
+        13, 14, 15, //
+
+        16, 17, 18, //
+        17, 18, 19, //
+
+        20, 21, 22, //
+        21, 22, 23  //
     };
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 5 * 4, &vertices[0], GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 2 * 3, &faces[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(faces), &faces[0], GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    Texture grassTexture("assets/grass.jpg");
+    Texture grassTexture("assets/tileset.jpg");
     Shader shader("srcs/shaders/shader.vs", "srcs/shaders/shader.fs");
 
     while (!glfwWindowShouldClose(window))
@@ -102,7 +146,7 @@ void WindowManager::updateLoop()
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, grassTexture.getID());
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 2 * 3, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(faces) / sizeof(unsigned int) * 3, GL_UNSIGNED_INT, 0);
 
         Time::updateTime();
         glfwSwapBuffers(window);
