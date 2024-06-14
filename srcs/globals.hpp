ac95893902cf17c3ea252076c600de3fcf2df62b
moi <not@me>
2024-06-15 01:27:32 +0200

    clean globals.hpp

--- srcs/globals.hpp
+++ srcs/globals.hpp
@@ -1,33 +1,36 @@
 #pragma once
 
-//@todo clean unused globals and sort them by files
-
-constexpr int TILESET_WIDTH = 2;
-constexpr int TILESET_HEIGHT = 2;
-
+// window
 constexpr int WINDOW_WIDTH = 720;
 constexpr int WINDOW_HEIGHT = 480;
 
-constexpr int RENDER_DISTANCE = 1;
-constexpr int RENDER_DISTANCE_2X = 2 * RENDER_DISTANCE + 1;
-constexpr int FOV = 80;
-
-constexpr int WORLD_LENGTH = 256; // ?
-
-constexpr int CHUNK_LENGTH = 16;
-constexpr int CHUNK_LENGTH_PLUS_2 = CHUNK_LENGTH + 2;
-constexpr int CHUNK_HEIGHT = 256;
-constexpr int CHUNK_ARRAY_SIZE = CHUNK_LENGTH_PLUS_2 * CHUNK_LENGTH_PLUS_2 * CHUNK_HEIGHT;
-constexpr int HALF_CHUNK_HEIGHT = CHUNK_HEIGHT / 2;
-constexpr int NB_CHUNK_ON_EDGE = WORLD_LENGTH / CHUNK_LENGTH;
-
-constexpr int DAY_DURATION_IN_MIN = 20;
-constexpr int DAY_DURATION_IN_SEC = DAY_DURATION_IN_MIN * 60;
-
+// text
 constexpr int TEXT_PIXEL_SIZE = 48;
 constexpr float CHAT_DISPLAY_TIME = 5;
 constexpr float CHAT_FADE_TIME = 5;
 
+// camera
+constexpr int RENDER_DISTANCE = 1;
+constexpr int RENDER_DISTANCE_2X = 2 * RENDER_DISTANCE + 1;
+constexpr int FOV = 80;
+
+// world
+constexpr int CHUNK_LENGTH = 16;
+constexpr int CHUNK_LENGTH_PLUS_2 = CHUNK_LENGTH + 2;
+constexpr int CHUNK_HEIGHT = 256;
+constexpr int CHUNK_ARRAY_SIZE = CHUNK_LENGTH_PLUS_2 * CHUNK_LENGTH_PLUS_2 * CHUNK_HEIGHT;
+constexpr int WORLD_LENGTH = 256; // should be 16384 but need to be update when I do generation
+constexpr int WATER_LEVEL = 80;
+
+// textures
+constexpr int TILESET_WIDTH = 2;
+constexpr int TILESET_HEIGHT = 2;
+
+// gameplay
+constexpr int DAY_DURATION_IN_MIN = 20;
+constexpr int DAY_DURATION_IN_SEC = DAY_DURATION_IN_MIN * 60;
+
+/*
 constexpr int CONTINENTALNESS_OCTAVES = 8;
 constexpr float CONTINENTALNESS_FREQUENCY = 4;
 constexpr float CONTINENTALNESS_PERSISTENCE = 0.3;
@@ -42,5 +45,4 @@ constexpr int PV_OCTAVES = 8;
 constexpr float PV_FREQUENCY = 8;
 constexpr float PV_PERSISTENCE = 0.1;
 constexpr int PV_ROUND_FACTOR = 5;
-
-constexpr int WATER_LEVEL = 80;
+*/
